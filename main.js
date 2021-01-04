1e28e9927dd91367f01114dff1e51a00c2d62268
yujin mac <kimyu28jin@gmail.com>
2021-01-04 23:20:51 +0900

    closeModal 함수 추가

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const plusBtn = document.querySelector(".main_add_btn");
 const modalSection = document.querySelector(".modal_section");
-
+const closeBtn = document.querySelector(".close");
 const modalOpen = () => {
   modalSection.classList.remove("hide");
 };
 
+const closeModal = () => {
+  modalSection.classList.add("hide");
+};
+
 plusBtn.addEventListener("click", modalOpen);
+closeBtn.addEventListener("click", closeModal);
