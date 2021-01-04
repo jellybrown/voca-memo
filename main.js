21ec40fbb28fe83fc91274fafff203faffdcd7e0
yujin mac <kimyu28jin@gmail.com>
2021-01-05 00:00:53 +0900

    addNewWord 함수 생성

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+import Word from "./word.js";
+
 const plusBtn = document.querySelector(".main_add_btn");
 const modalSection = document.querySelector(".modal_section");
 const closeBtn = document.querySelector(".close");
+const content = document.querySelector(".content");
+const modalAddBtn = document.querySelector(".modal_add_btn");
+const engInput = document.querySelector("#eng_input");
+const koInput = document.querySelector("#ko_input");
+
 const openModal = () => {
   modalSection.classList.remove("hide");
 };
@@ -11,3 +18,15 @@ const closeModal = () => {
 
 plusBtn.addEventListener("click", openModal);
 closeBtn.addEventListener("click", closeModal);
+
+let inputs = document.querySelectorAll("input");
+
+const addNewWord = () => {
+  const engValue = engInput.value;
+  const koValue = koInput.value;
+  const word = new Word(engValue, koValue);
+  console.log(word);
+};
+
+modalAddBtn.addEventListener("click", addNewWord);
+//inputs.addEventListener("keypress", inputListener);
--- word.js
+++ word.js
@@ -1,6 +1,8 @@
-export class Word {
+class Word {
   constructor(eng, ko) {
     this._eng = eng;
     this._ko = ko;
   }
 }
+
+export default Word;
